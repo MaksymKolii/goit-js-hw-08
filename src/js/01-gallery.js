23e92b230d95efce6376acfb1f6ad0336e1f4716
Maksym Kolii <maksymkolii@gmail.com>
2022-09-21 14:45:31 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+console.log(SimpleLightbox);
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems[0].original);
+
+const galleryElRef = document.querySelector(".gallery");
+
+function makeGalleryMarkup(object) {
+  return object
+    .map(
+      ({ original, preview, description }) => `<li class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                   <img
+                      class="gallery__image"
+                      src="${preview}"
+                      alt="${description}"
+                  />
+              </a>
+          </li>`
+    )
+    .join("");
+}
+galleryElRef.insertAdjacentHTML("beforeend", makeGalleryMarkup(galleryItems));
+
+new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
