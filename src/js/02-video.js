f2406625ea2c02a46d90177ca8548e889698dc12
Maksym Kolii <maksymkolii@gmail.com>
2022-09-22 15:08:00 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,29 +4,12 @@ import Player from '@vimeo/player';
 
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// //player = new Vimeo.Player(iframe);
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
