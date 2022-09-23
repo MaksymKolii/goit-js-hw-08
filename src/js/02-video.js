cac82d32d774c933588cc08e85792df5a1c90d75
Maksym Kolii <maksymkolii@gmail.com>
2022-09-23 23:08:48 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,39 +6,20 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const getSavedTime = localStorage.getItem('videoplayer-current-time');
 console.log(getSavedTime);
-// console.log(typeof(+currentSavedTime));
 
-// const parsedCurrentSavedTime = JSON.parse(getSavedTime);
+player.on('timeupdate',  throttle(updatedTime, 1000));
 
-player.on('timeupdate',  throttle(checkedTime, 1000));
 
-function checkedTime(data){
+function updatedTime({seconds}){
+  const currentTime = Math.round(seconds)
     localStorage.setItem(
       'videoplayer-current-time',
-      `${data.seconds}`
+      currentTime
     );
-    console.log(data.seconds);
-      console.log(typeof(data.seconds));
 }
 
 player.setCurrentTime(getSavedTime)
 
-// player.setCurrentTime(getSavedTime).then(function(seconds) {
-//   console.log("seconds -:?", seconds);
-//   if(seconds>=562){
-//     console.log('the time was less than 0 or greater than the video’s duration');
-    
-//   }
-// }).catch(function(error) {
-//   switch (error.name) {
-//       case 'RangeError':
-//           // the time was less than 0 or greater than the video’s duration
-//           break;
 
-//       default:
-//           // some other error occurred
-//           break;
-//   }
-// });
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+localStorage.removeItem("videoplayer-current-time");
 
+const LOCALSTORAGE_KEY="feedback-form-state"
+
+// const refs={
+
+//     inputMail: document.querySelector('input[name="email"]'),
+//     inputMessage: document.querySelector('textarea[name="message"]'),
+//     form: document.querySelector('.feedback-form'),
+//     LOCALSTORAGE_KEY:"feedback-form-state",
+// }
+// refs.inputMail.addEventListener('input', onInputChange);
+// refs.inputMessage.addEventListener('input', onInputChange);
+// refs.form.addEventListener('submit', onFormSubmit)
+
+
+function onFormSubmit(e){
+    e.preventDefault();
+    const {
+      elements: { email, message },
+    } = e.currentTarget;
+    // if (!email.value || !message.value) {
+    //     return window.alert('Please input data!');
+    //   }
+      const userData = { email: email.value, message: message.value };
+
+      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
+    console.log(userData);
+
+      e.currentTarget.reset();
+      localStorage.removeItem("feedback-form-state");
+}
+
+
+
+
+const formInput =document.querySelector('.feedback-form')
+const formSubmit =document.querySelector('.feedback-form')
+
+formInput.addEventListener('input', onInputfillLocalStorage)
+formSubmit.addEventListener('submit', onFormSubmit)
+
+
+function onInputfillLocalStorage(e){
+
+    e.preventDefault();
+    
+    const {
+      elements: { email, message },
+    } = e.currentTarget;
+  
+    console.log(email.value, message.value);
+
+      const userData = { email: email.value, message: message.value };
+
+      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
+    console.log(userData);
+
+      //e.currentTarget.reset();
+}
