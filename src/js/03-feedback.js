1e2cd04e0fa4098e5e55f93c33ec65e714a12cee
Maksym Kolii <maksymkolii@gmail.com>
2022-09-25 12:10:54 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,85 +1,59 @@
 import throttle from 'lodash.throttle';
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-
 const refs = {
-  inputData: document.querySelector('.feedback-form'),
+  //input: document.querySelector('.feedback-form input'),
   form: document.querySelector('.feedback-form'),
+  //textarea: document.querySelector('.feedback-form textarea'),
 };
 
-refs.inputData.addEventListener('input', throttle(onInputChange, 500));
+refs.form.addEventListener('input', throttle(onTextInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
-populatedInputData();
+let userData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
 
-function onInputChange(e) {
-  const {
-    elements: { email, message },
-  } = e.currentTarget;
-  console.log(email.value, message.value);
+//* первый метод для которого нужны слушатели на textarea и input
+//populatedInputData(userData);
 
-//   const userData = { email: email.value, message: message.value };
-  const userData = { email: email.value, message: message.value };
 
+//* Второй метод для которого нужен только слкшатель на formе
+populatedInputDataNew(userData);
+
+function onTextInput(e) {
+  userData[e.target.name] = e.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
-  console.log(userData);
-  console.log(localStorage);
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
-  //console.log('Sending form');
   e.currentTarget.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
-function populatedInputData() {
-  const savedInputData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  if (savedInputData) {
-    refs.inputData.email.value = savedInputData.email;
-    refs.inputData.message.value = savedInputData.message;
+function populatedInputData(data) {
+  if (data) {
+    if (data['email'] !== undefined) {
+      refs.input.value = data['email'];
+    } else {
+      refs.input.value = '';
+    }
+    if (data['message'] !== undefined) {
+      refs.textarea.value = data['message'];
+    } else {
+      refs.textarea.value = '';
+    }
   }
 }
 
-//2=====================================================================================
+/* 
+1) Из localStorage методом Object.entries(data)-- получаем массив масивов  со значением ключ, значение [name, value]
+2)  проходимся forEach по массиву [ ["email","rr@kkk.com"], ["message","yfvyuv"] ] и вставляем в нашу ссылку на 
+элемент формы [name].value значение(value) из Форыча
+*/
+function populatedInputDataNew(data) {
 
-// import throttle from 'lodash.throttle';
-// localStorage.removeItem('videoplayer-current-time');
-
-// const LOCALSTORAGE_KEY = 'feedback-form-state';
-
-// const inputData = document.querySelector('.feedback-form');
-// const form = document.querySelector('.feedback-form');
-
-// inputData.addEventListener('input', throttle(onTextInput, 500));
-// form.addEventListener('submit', onFormSubmit);
-
-
-// populatedInputData();
-// const userData ={};
-
-// function onTextInput(e) {
-
-//   userData[e.target.name] = e.target.value;
-
-//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
-//   console.log(userData);
-//   console.log(localStorage);
-// }
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   e.currentTarget.reset();
-//   localStorage.removeItem(LOCALSTORAGE_KEY);
-// }
-
-// function populatedInputData() {
-//   const savedInputData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-
-//   if (savedInputData) {
-//     inputData.email.value = savedInputData.email;
-//     // console.log('savedInputData.email', savedInputData.email);
-//     inputData.message.value = savedInputData.message;
-//     // console.log('savedInputData.message', savedInputData.message);
-    
-//   }
-// }
+  if (data) {
+    Object.entries(data).forEach(([name, value]) => {
+      refs.form.elements[name].value = value;
+    });
+  }
+}
